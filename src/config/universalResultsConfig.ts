2b0507096eab8fe3c8938b31147c884477eae581
ds-mansi <mansi.gurnani@dotsquares.com>
2023-05-13 12:12:54 +0530

    section updated

--- src/config/universalResultsConfig.ts
+++ src/config/universalResultsConfig.ts
@@ -22,7 +22,7 @@ export const universalResultsConfig: UniversalResultsConfig = {
     }
   },
   articles: {
-    SectionComponent:ProductsSection,
+    // SectionComponent:ProductsSection,
     label: 'Articles',
     viewAllButton: true,
     cardConfig: {
